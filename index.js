4cf514f9595796cd6c0dc56b8205bf26158d49ed
Oanishka <81215857+Oanishka@users.noreply.github.com>
2022-03-17 18:20:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+function myFunction1() {
+
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Citeste mai mult";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Citeste mai putin";
+    moreText1.style.display = "inline";
+  }
+};
+
+
+
+function myFunction2() {
+
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Citeste mai mult";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Citeste mai putin";
+    moreText2.style.display = "inline";
+  }
+};
+
+
+function myFunction3() {
+
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText3 = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "Citeste mai mult";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "Citeste mai putin";
+    moreText3.style.display = "inline";
+  }
+};
+
+
+
+
+function myFunction4() {
+
+  var dots4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("more4");
+  var btnText4 = document.getElementById("myBtn4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "Citeste mai mult";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "Citeste mai putin";
+    moreText4.style.display = "inline";
+  }
+};
+
+
+function myFunction5() {
+
+  var dots4 = document.getElementById("dots5");
+  var moreText4 = document.getElementById("more5");
+  var btnText4 = document.getElementById("myBtn5");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "Citeste mai mult";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "Citeste mai putin";
+    moreText4.style.display = "inline";
+  }
+};
